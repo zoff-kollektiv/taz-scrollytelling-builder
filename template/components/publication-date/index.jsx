e9d7939e69ce2894993aa4208cca0912461750b9
Gustav Pursche <pursche@posteo.de>
2018-10-10 12:21:41 +0200

    Add missing components

---
+++ template/components/publication-date/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default {
+  name: 'publication-date',
+  private: true,
+  Component: props => <small>{props.children}</small>
+};
---
+++ template/components/standfirst/styles.js
@@ -0,0 +1,10 @@
+import css from 'styled-jsx/css';
+
+export default css`
+  .standfirst {
+    font-family: Poppins, Georgia;
+    font-size: 1.3rem;
+    line-height: 1.5;
+    text-align: center;
+  }
+`;
