49a4b8c631e57b7358b780f14941ca8ce7ae1871
Gustav Pursche <pursche@posteo.de>
2018-10-11 09:03:52 +0200

    Paragraph: add serializer

--- template/components/paragraph/index.jsx
+++ template/components/paragraph/index.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 import Icon from './paragraph.svg';
 import styles from './styles';
 
+const Paragraph = ({ attributes = {}, children }) => (
+  <p className="paragraph" {...attributes}>
+    <style jsx>{styles}</style>
+    {children}
+  </p>
+);
+
 export default {
   name: 'paragraph',
   Icon,
   private: true,
   styles,
-  Component: ({ attributes, children }) => (
-    <p className="paragraph" {...attributes}>
-      <style jsx>{styles}</style>
-      {children}
-    </p>
+  Component: ({ node, ...rest }) => <Paragraph data={node.data} {...rest} />,
+
+  serialize: (node, children) => (
+    <Paragraph data={node.data}>{children}</Paragraph>
   )
 };
