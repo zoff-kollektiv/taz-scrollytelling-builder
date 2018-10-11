9f245236a6de6054f265f52372a1aa540a842a3b
Gustav Pursche <pursche@posteo.de>
2018-10-11 09:03:40 +0200

    Blockquote: add serializer

--- template/components/blockquote/index.jsx
+++ template/components/blockquote/index.jsx
@@ -39,22 +39,28 @@ const BLOCK_DEFINITION = {
   ]
 };
 
+const Blockquote = ({ attributes = {}, children }) => (
+  <blockquote className="blockquote" {...attributes}>
+    <style jsx>{styles}</style>
+
+    <LayoutConstraint.Component>
+      <div className="icon-container">
+        <Icon style={{ height: '3rem', width: '3rem' }} />
+      </div>
+
+      {children}
+    </LayoutConstraint.Component>
+  </blockquote>
+);
+
 export default {
   name: 'blockquote',
   Icon,
   styles,
-  Component: ({ attributes, children }) => (
-    <blockquote className="blockquote" {...attributes}>
-      <style jsx>{styles}</style>
+  Component: ({ node, ...rest }) => <Blockquote data={node.data} {...rest} />,
 
-      <LayoutConstraint.Component>
-        <div className="icon-container">
-          <Icon style={{ height: '3rem', width: '3rem' }} />
-        </div>
-
-        {children}
-      </LayoutConstraint.Component>
-    </blockquote>
+  serialize: (node, children) => (
+    <Blockquote data={node.data}>{children}</Blockquote>
   ),
 
   insert(change) {
