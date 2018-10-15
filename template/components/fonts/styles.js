7b6dbb056a9efb30a2e1e5fce70eb5f42c826493
Gustav Pursche <pursche@posteo.de>
2018-10-15 13:42:21 +0200

    Fonts: remove local lookups

--- template/components/fonts/styles.js
+++ template/components/fonts/styles.js
@@ -25,9 +25,6 @@ export default css`
     font-style: normal;
     font-weight: 400;
     src:
-      local('Poppins'),
-      local('Poppins Regular'),
-      local('Poppins-Regular'),
       url('${path}/poppins.woff') format('woff'),
       url('${path}/poppins.ttf') format('truetype');
   }
@@ -38,8 +35,6 @@ export default css`
     font-style: normal;
     font-weight: 700;
     src:
-      local('Poppins Bold'),
-      local('Poppins-Bold'),
       url('${path}/poppins-bold.woff') format('woff'),
       url('${path}/poppins-bold.ttf') format('truetype');
   }
@@ -50,8 +45,6 @@ export default css`
     font-style: italic;
     font-weight: 400;
     src:
-      local('Poppins Italic'),
-      local('Poppins-Italic'),
       url('${path}/poppins-italic.woff') format('woff'),
       url('${path}/poppins-italic.ttf') format('truetype');
   }
@@ -61,10 +54,7 @@ export default css`
     font-family: 'Lora';
     font-style: normal;
     font-weight: 400;
-    src: local('Lora'),
-         local('Lora Regular'),
-         local('Lora-Regular'),
-         url(${path}/lora.woff) format('woff'),
+    src: url(${path}/lora.woff) format('woff'),
          url(${path}/lora.ttf) format('truetype');
   }
 
@@ -73,9 +63,7 @@ export default css`
     font-family: 'Lora';
     font-style: normal;
     font-weight: 700;
-    src: local('Lora Bold'),
-         local('Lora-Bold'),
-         url(${path}/lora-bold.woff) format('woff'),
+    src: url(${path}/lora-bold.woff) format('woff'),
          url(${path}/lora-bold.ttf) format('truetype');
   }
 
@@ -84,9 +72,7 @@ export default css`
     font-family: 'Lora';
     font-style: italic;
     font-weight: 700;
-    src: local('Lora Bold Italic'),
-         local('Lora-BoldItalic'),
-         url(${path}/lora-bold-italic.woff) format('woff'),
+    src: url(${path}/lora-bold-italic.woff) format('woff'),
          url(${path}/lora-bold-italic.ttf) format('truetype');
   }
 
@@ -95,9 +81,7 @@ export default css`
     font-family: 'Lora';
     font-style: italic;
     font-weight: 400;
-    src: local('Lora Italic'),
-         local('Lora-Italic'),
-         url(${path}/lora-italic.woff) format('woff'),
+    src: url(${path}/lora-italic.woff) format('woff'),
          url(${path}/lora-italic.ttf) format('truetype');
   }
 `;
