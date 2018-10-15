6feef4b032be9ec9d2bc2b8d004ede1b0876c4f7
Gustav Pursche <pursche@posteo.de>
2018-10-15 22:26:00 +0200

    Modal: add close button

--- editor/components/modal/index.jsx
+++ editor/components/modal/index.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+import CloseIcon from './times.svg';
 import styles from './styles';
 
 ReactModal.setAppElement('#builder');
 
-export default ({ children }) => (
-  <ReactModal isOpen className="modal" overlayClassName="modal-overlay">
-    <style jsx>{styles}</style>
-    {children}
-  </ReactModal>
-);
+export default class Modal extends Component {
+  state = {
+    showModal: true
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <ReactModal
+        isOpen={this.state.showModal}
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <style jsx>{styles}</style>
+
+        <header className="header">
+          <button
+            type="button"
+            className="close"
+            onClick={this.handleCloseModal}
+          >
+            <span className="close__label">Close Modal</span>
+            <CloseIcon style={{ height: '1rem', width: '1rem' }} />
+          </button>
+        </header>
+
+        <div className="content">{children}</div>
+      </ReactModal>
+    );
+  }
+}
