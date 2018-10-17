dce7450aa0ad7a5d0c605d3e87909bddb1dfe71c
Gustav Pursche <pursche@posteo.de>
2018-10-17 22:09:52 +0200

    Intro: mark as non-editable

--- template/components/intro/index.jsx
+++ template/components/intro/index.jsx
@@ -25,6 +25,7 @@ export default {
   name: 'intro',
   private: true,
   styles,
+  canEdit: false,
   Component: ({ node, ...rest }) => <Intro data={node.data} {...rest} />,
 
   serialize(node, children) {
