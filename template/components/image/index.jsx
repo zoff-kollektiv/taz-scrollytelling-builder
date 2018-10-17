7d0bee88925933404223688c246eabac85d53a0e
Gustav Pursche <pursche@posteo.de>
2018-10-17 20:12:49 +0200

    Image: allow caption to be edited inline

--- template/components/image/index.jsx
+++ template/components/image/index.jsx
@@ -9,9 +9,29 @@ import layoutConstraint from '../layout-constraint';
 import Radio from '../../../editor/components/form/radio';
 import styles from './styles';
 
+const BLOCK_DEFINITION = {
+  object: 'block',
+  type: 'image',
+  nodes: [
+    {
+      object: 'block',
+      type: 'image-caption',
+      nodes: [
+        {
+          object: 'text',
+          leaves: [
+            {
+              text: 'Caption'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
 const Image = ({ data, attributes = {}, children, serialize = false }) => {
   const alt = data.get('alt') || '';
-  const caption = data.get('caption');
   const type = data.get('type');
   const width = data.get('image_width');
   const height = data.get('image_height');
@@ -46,13 +66,9 @@ const Image = ({ data, attributes = {}, children, serialize = false }) => {
       {/* eslint-disable-next-line jsx-a11y/alt-text */}
       <img {...attrs} />
 
-      {caption && (
-        <layoutConstraint.Component>
-          <figcaption className="caption">{caption}</figcaption>
-        </layoutConstraint.Component>
-      )}
-
-      {children}
+      <layoutConstraint.Component>
+        <div className="caption-container">{children}</div>
+      </layoutConstraint.Component>
     </figure>
   );
 };
@@ -101,7 +117,6 @@ export default {
           e.g. use a service like https://tinypng.com.
         `}
         />,
-        <Input name="caption" label="Caption" />,
         <Input name="alt" label="Alt" />,
         <Radio
           name="type"
@@ -114,5 +129,12 @@ export default {
         />
       ]
     };
+  },
+
+  insert(change, data) {
+    // pass data to block
+    BLOCK_DEFINITION.data = data;
+
+    change.insertBlock(BLOCK_DEFINITION);
   }
 };
