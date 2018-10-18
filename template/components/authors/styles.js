9ce2a946d1eba6d8a9e3d5acf50f2d74d73071ba
Gustav Pursche <pursche@posteo.de>
2018-10-18 13:32:43 +0200

    Authors: increase font-size on small screens

--- template/components/authors/styles.js
+++ template/components/authors/styles.js
@@ -5,7 +5,7 @@ import { fonts, mq } from '../../tokens';
 export default css`
   .authors {
     font-family: ${fonts.sansSerif.family};
-    font-size: 0.75rem;
+    font-size: 0.95rem;
     font-style: italic;
     letter-spacing: 0.06rem;
     line-height: 1.2;
