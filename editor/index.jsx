b4cb77e6e7e937dc0317d086bcc3ae4e8b0395f1
Gustav Pursche <pursche@posteo.de>
2018-10-19 20:43:31 +0200

    Lint

--- editor/index.jsx
+++ editor/index.jsx
@@ -49,7 +49,7 @@ class Application extends Component {
                       ]
                     }
                   ]
-                },
+                }
               ]
             }
           ]
