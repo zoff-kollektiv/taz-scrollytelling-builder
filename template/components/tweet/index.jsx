7b671a0958a1071f58a700754e556479eb6f344c
Gustav Pursche <pursche@posteo.de>
2018-11-28 06:54:26 +0100

    Tweet: properly inject script tag into editor

--- template/components/tweet/index.jsx
+++ template/components/tweet/index.jsx
@@ -1,13 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Textarea from '../../../editor/components/form/textarea';
 
-const Tweet = ({ data, children }) => (
-  <Fragment>
-    <div dangerouslySetInnerHTML={{ __html: data.get('embedCode') }} />
-    {children}
-  </Fragment>
-);
+class Tweet extends Component {
+  // inject twitters script tag, which then takes care of the tweet rendering
+  componentDidMount() {
+    const script = document.createElement('script');
+
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+
+    if (document && document.body) {
+      document.body.appendChild(script);
+    }
+  }
+
+  render() {
+    const { data, children } = this.props;
+    const embedCode = data.get('embedCode');
+
+    return (
+      <Fragment>
+        <div dangerouslySetInnerHTML={{ __html: embedCode }} />
+        {children}
+      </Fragment>
+    );
+  }
+}
 
 export default {
   name: 'tweet',
